2aa66d8e9a6a9c9374db34630e5edc7cf268e8ff
Helio Argentoni <helio.argentoni@gmail.com>
2019-02-09 17:53:18 -0200

    Receber_entrada_do_usuário_comando_read

---
+++ CursoShellScript/Scripts/BuscaAluno.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##############################################################
+# Buscaaluno.sh - Procurar nome completo do aluno
+# Autor: Hélio Argentoni
+#Data Crianção: 09/02/2019
+#Descrição: Script de exemplo para o comando read.
+#	    A partir de uma entrada do usuário, será feita uma
+#	    busca no arquivo aluno2.txt
+#Exemplo de uso: ./Buscaaluno.sh
+##############################################################
+
+######      Forma 1 de fazer esse script    ########
+
+# ARQALUNOS="/home/vasconcelos/Documents/cursos/ShellScriptUdemy/CursoShellScript/arquivos/alunos2.txt"
+# clear
+# echo "=======Scrtipt de Busca de Aluno========"
+# echo ""
+# echo -n "Por favor, informe o nome do aluno: "
+# read ALUNO
+# echo ""
+# echo "O nome completo do aluno é: $( grep "$ALUNO" "$ARQALUNOS" ) "
+# echo ""
+# echo "Fim do Script"
+
+##### Forma 2 de fazer esse script #######
+
+ARQALUNOS="/home/vasconcelos/Documents/cursos/ShellScriptUdemy/CursoShellScript/arquivos/alunos2.txt"
+clear
+echo "=======Scrtipt de Busca de Aluno========"
+echo ""
+read -p "Por favor, informe o nome do aluno: " ALUNO
+echo""
+ALUNONOMECOMPLETO=$(grep "$ALUNO" "$ARQALUNOS")
+echo""
+echo "O nome completo do aluno é: $ALUNONOMECOMPLETO"
+echo ""
+echo "Fim do Script"

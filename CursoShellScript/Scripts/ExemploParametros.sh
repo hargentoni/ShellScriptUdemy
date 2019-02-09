16427083a5a1e74c233137343ef4e4687c9766e7
Helio Argentoni <helio.argentoni@gmail.com>
2019-02-09 18:20:00 -0200

    ExemploParametros

---
+++ CursoShellScript/Scripts/ExemploParametros.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $0 - Nome do programa ou no caso, o nome do script
+# $# - Quantidade de Parâmetros Recebidos
+# $* - Exibe quais foram os Parâmetros recebidos (todos)
+# $1-9 ou de 1 até 9 - Mostra cada ums dos parâmetros recebidos
+
+echo "O script $0 recebeu $# parâmetros"
+echo ""
+echo "Os parâmetros recebidos foram: $*"
+echo ""
+echo "Parâmetro \$1 = $1"
+echo "Parâmetro \$2 = $2"
+echo "Parâmetro \$3 = $3"
+echo "Parâmetro \$4 = $4"
+echo "Parâmetro \$5 = $5"
+
+
+
+
+
